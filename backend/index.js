673b86608f98b72e3a2b268fb4b24db612a373aa
Priyaal Gayakwad <priyaalgayakwad3@gmail.com>
2026-07-30 12:23:28 +0530

    updated initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    rigin: [
+    origin: [
         "http://localhost:5173",
         "https://react-portfolio-alpha-one-12.vercel.app",
     ],
