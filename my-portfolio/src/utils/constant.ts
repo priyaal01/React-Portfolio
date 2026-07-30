6eac9ee480b6af7adf00c3ae7400eb3f4e78223a
Priyaal Gayakwad <priyaalgayakwad3@gmail.com>
2026-07-30 13:53:20 +0530

    constant.ts

--- my-portfolio/src/utils/constant.ts
+++ my-portfolio/src/utils/constant.ts
@@ -1,4 +1,1 @@
-export const RESEND_API_END_POINT =
-    import.meta.env.VITE_API_URL || "NO_ENV_FOUND";
-
-console.log("API URL =", RESEND_API_END_POINT);
+export const RESEND_API_END_POINT = import.meta.env.production.VITE_API_URL || "NO_ENV_FOUND";
